e8cf1e21caa94e1f0692cc6fc3ee33e5c56d4624
Paul <eublefar@gmail.com>
2022-10-08 21:48:41 +0300

    done currency converter

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Wrapper from '../UI/Wrapper/Wrapper';
+import CurrencyConverter from '../CurrencyConverter/CurrencyConverter';
+
+function App() {
+  return (
+    <Wrapper>
+      <CurrencyConverter />
+    </Wrapper>
+  );
+}
+
+export default App;
